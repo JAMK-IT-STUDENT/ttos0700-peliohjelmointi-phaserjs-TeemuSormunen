702911d5b4593556fa9b732d86798353ebc10bf7
TeemuSormunen <K9414@student.jamk.fi>
2018-01-23 20:41:17 +0200

    add states

---
+++ PhaserDemo/state3.js
@@ -0,0 +1,8 @@
+demo.state3 = function(){};
+demo.state3.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor ='#d424b3'
+    },
+    update: function(){}
+};
