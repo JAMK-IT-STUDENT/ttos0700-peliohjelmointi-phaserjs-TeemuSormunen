1e1c65f1845df640d140acb37e101f51786e7d94
TeemuSormunen <K9414@student.jamk.fi>
2018-02-02 18:24:56 +0200

    Add assets + arrow movement

--- PhaserDemo/state0.js
+++ PhaserDemo/state0.js
@@ -1,16 +1,38 @@
-var demo = {};
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, ukko, speed = 4;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    
+    preload: function(){
+     game.load.image('ukko', 'Assets/Sprites/ukko.png');
+        
+    },
+    
+    
     create: function(){
         game.stage.backgroundColor ='#3e668e';
         console.log('state0');
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
+        ukko = game.add.sprite(centerX, centerY, 'ukko');
+        ukko.anchor.setTo(0.5, 0.5);
         
     },
-    update: function(){}
+    update: function(){
+        
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            ukko.x += speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            ukko.x -= speed; 
+        } 
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            ukko.y -= speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            ukko.y += speed; 
+        } 
+    }
 };
 
 function changeState(i, stateNum){
@@ -22,7 +44,7 @@ function addKeyCallback(key, fn, args){
     game.input.keyboard.addKey(key).onDown.add(fn, null, null, args);
 }
 function addChangeStateEventListeners(){
-            addKeyCallback(Phaser.Keyboard.ZERO,changeState, 0);
+        addKeyCallback(Phaser.Keyboard.ZERO,changeState, 0);
         addKeyCallback(Phaser.Keyboard.ONE,changeState, 1);
         addKeyCallback(Phaser.Keyboard.TWO,changeState, 2);
         addKeyCallback(Phaser.Keyboard.THREE,changeState, 3);
