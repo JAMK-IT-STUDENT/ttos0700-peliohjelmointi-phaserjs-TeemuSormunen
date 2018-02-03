bf3b5d2e9647e64a13d07834811b72dd111628e3
TeemuSormunen <K9414@student.jamk.fi>
2018-02-03 16:29:01 +0200

    Add animation, tilemap

--- PhaserDemo/state0.js
+++ PhaserDemo/state0.js
@@ -1,37 +1,65 @@
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, ukko, speed = 4;
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, ukko, speed = 6;
 demo.state0 = function(){};
 demo.state0.prototype = {
     
-    preload: function(){
-     game.load.image('ukko', 'Assets/Sprites/ukko.png');
-        
+    preload: function() {
+    game.load.spritesheet('ukko', 'Assets/spritesheet/ukko.png',259, 470);
+    game.load.image('forest', 'Assets/backgrounds/Background1.png');
+          
     },
     
-    
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor ='#3e668e';
         console.log('state0');
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        var forestBG = game.add.sprite(0, 0, 'forest');
         
         ukko = game.add.sprite(centerX, centerY, 'ukko');
         ukko.anchor.setTo(0.5, 0.5);
+        game.physics.enable(ukko);
+        ukko.body.collideWorldBounds = true;
+        ukko.animations.add('walk',[0,1]);
+        ukko.scale.setTo(0.6, 0.6);
         
+        game.camera.follow(ukko);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000 );
     },
     update: function(){
         
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+                ukko.scale.setTo(0.6, 0.6);
             ukko.x += speed;
+            ukko.animations.play('walk',7,true);
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            ukko.scale.setTo(-0.6, 0.6);
             ukko.x -= speed; 
+            ukko.animations.play('walk',7,true);
         } 
+        else{
+            ukko.animations.stop();
+            ukko.frame = 0;
+        }
+        
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             ukko.y -= speed;
+            if(ukko.y < 440 ){
+               ukko.y = 440;
+                ukko.animations.play('walk',7,true);
+               }
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             ukko.y += speed; 
+            if(ukko.y > 734 ){
+               ukko.y = 734;
+                ukko.animations.play('walk',7,true);
         } 
+
+        }
     }
 };
 
--- PhaserDemo/state1.js
+++ PhaserDemo/state1.js
@@ -1,11 +1,53 @@
 
 demo.state1 = function(){};
+var cursors, vel = 500, rocks, grass;
 demo.state1.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.tilemap('field', 'Assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('grass', 'Assets/tilemaps/grass.png');
+        game.load.image('stone2', 'Assets/tilemaps/stone2.png');
+        game.load.spritesheet('ukko', 'Assets/spritesheet/ukko.png',259, 470);
+    },
     create: function(){
         game.stage.backgroundColor ='#3ba24f'
         console.log('state1');
         addChangeStateEventListeners();
+        var map = game.add.tilemap('field');
+        map.addTilesetImage('grass');
+        map.addTilesetImage('stone2');
+        
+        map.setCollisionBetween(1,9, true, 'rocks');
+        map.setCollision(11, true, 'grass');
+        
+        grass = map.createLayer('grass');
+        rocks = map.createLayer('rocks');
+        ukko = game.add.sprite(200, 200, 'ukko');
+        ukko.scale.setTo(0.2, 0.2);
+        game.physics.enable(ukko);
+        
+        cursors = game.input.keyboard.createCursorKeys();
     },
-    update: function(){}
+    update: function(){
+        game.physics.arcade.collide(ukko, rocks,function(){console.log("Hitting rocks")});
+        game.physics.arcade.collide(ukko, grass,function(){console.log("Hitting grass")});
+        if(cursors.up.isDown){
+            ukko.body.velocity.y = -vel;
+        }
+        else if(cursors.down.isDown){
+            ukko.body.velocity.y = vel;
+        }
+        else{
+            ukko.body.velocity.y = 0;
+        }
+        if(cursors.left.isDown){
+            ukko.body.velocity.x = -vel;
+        }
+        else if(cursors.right.isDown){
+            ukko.body.velocity.x = vel;
+        }
+        else{
+            ukko.body.velocity.x = 0;
+        }
+        
+    }
 };
