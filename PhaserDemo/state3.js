b1d4fe1c99f5c4f13ed533720e869768e3ea9060
TeemuSormunen <K9414@student.jamk.fi>
2018-02-04 13:30:06 +0200

    add state 3

--- PhaserDemo/state3.js
+++ PhaserDemo/state3.js
@@ -1,10 +1,45 @@
+var sound;
 demo.state3 = function(){};
 demo.state3.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('button1', 'Assets/Sprites/button1.png');
+        game.load.image('button2', 'Assets/Sprites/button2.png');
+        game.load.image('button3', 'Assets/Sprites/button3.png');
+        game.load.audio('pops', 'Assets/sounds/pop.mp3'); 
+    },
     create: function(){
         game.stage.backgroundColor ='#d424b3'
         console.log('state3');
         addChangeStateEventListeners();
+        
+        sound = game.add.audio('pops');
+        sound.addMarker('low',0.4, 0.6);
+        sound.addMarker('high',1.35,1.5);
+        
+        
+        var b1 = game.add.button(100, 100,'button1', function(){
+            changeState(null, 1);
+        });
+        var b2 = game.add.button(400, 400,'button2', function(){
+            changeState(null, 2);
+        });
+        var b3 = game.add.button(700, 700,'button3');
+        b1.onInputDown.add(this.tint, b1);
+        b2.onInputDown.add(this.tint, b2);
+        b3.onInputDown.add(this.tint, b3);
+        
+        b1.onInputUp.add(this.unTint, b1);
+        b2.onInputUp.add(this.unTint, b2);
+        b3.onInputUp.add(this.unTint, b3); 
+        
     },
-    update: function(){}
+   
+    tint: function(){
+        this.tint = 0xbbbbbb;
+        sound.play('low');
+    },
+    unTint: function(){
+        this.tint = 0xFFFFFF;
+        sound.play('high');
+    }
 };
