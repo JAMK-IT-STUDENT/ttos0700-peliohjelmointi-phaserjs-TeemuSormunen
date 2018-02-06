a95e91fceca065543828be5189a32884af71076d
TeemuSormunen <K9414@student.jamk.fi>
2018-02-06 12:50:06 +0200

    add state 8

--- PhaserDemo/state7.js
+++ PhaserDemo/state7.js
@@ -1,9 +1,46 @@
+var arrow, startPointX, startPointY, endPointX, endPointY, swipeDirection, leeway = 10;
 demo.state7 = function(){};
 demo.state7.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('arrow', 'Assets/Sprites/arrow.png');
+    },
     create: function(){
         console.log('state7');
         addChangeStateEventListeners();
+        
+        arrow = game.add.sprite(centerX, centerY, 'arrow');
+        arrow.anchor.setTo(0.5);
+        
+        game.input.onDown.add(this.startSwipe);
+        game.input.onUp.add(this.getSwipeDirection);
+        
     },
-    update: function(){}
+    update: function(){},
+    startSwipe: function(){
+        startPointX = game.input.x;
+        startPointY = game.input.y;
+    },
+    getSwipeDirection: function() {
+        endPointX = game.input.x;
+        endPointY = game.input.y;
+        if(Math.abs(endPointY - startPointY) < leeway && Math.abs(enpointX - startPointX) < leeway){
+            return false;
+        }
+        if (Math.abs(endPointY - startPointY) < Math.abs(endPointX - startPointX)) {
+            console.log('Horizontal');
+            if(endPointX > startPointX){
+                swipeDirection = 90;
+            } else {
+                swipeDirection = 270;
+            }
+        } else {
+            console.log('Vertical');
+            if (endPointY > startPointY){
+                swipeDirection = 180;
+            } else {
+                swipeDirection = 0;
+            }
+        }
+        arrow.angle = swipeDirection;
+    }
 };
--- PhaserDemo/state8.js
+++ PhaserDemo/state8.js
@@ -1,9 +1,43 @@
+var text;
+WebFontConfig = {
+google: { families: [ 'Nanum Pen Script' ] }
+};
+
 demo.state8 = function(){};
 demo.state8.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js')
+    },
     create: function(){
         console.log('state8');
         addChangeStateEventListeners();
+        text = 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultrices enim sem, quis interdum velit blandit eget. Suspendisse mollis est a lectus dictum ornare. Donec a suscipit magna. Sed mauris turpis,tristique quis ante ut, ullamcorper facilis nisl. proin quis orci faucibus. congue nisi ac, imperdies justo, Donec scelerisque, libero.';
+        
+        this.spellOutText(100, 100, 1000, text, 30, 40, '#fff', 'Nanum Pen Script');
+        
     },
-    update: function(){}
+    
+    spellOutText: function(x, y, width ,text, fontSize, speed, fill, font){
+        var sentence = game.add.text(x,y,'',{fontSize: fontSize + 'px',fill:fill,font:font});
+        var currentLine = game.add.text(10,10,'',{fontSize:fontSize +'px', font:font});
+        currentLine.alpha = 0;
+        var loop = game.time.events.loop(speed, addChar);
+        
+        var index = 0;
+        
+        function addChar() {
+            sentence.text += text[index];
+            currentLine.text += text[index];
+            
+            if(currentLine.width > width && text[index] == ' ') {
+                sentence.text += '\n';
+                currentLine.text = '';
+            }
+            if(index >= text.length - 1){
+                game.time.events.remove(loop);
+                console.log('stop');
+            }
+            index++;
+        }
+    }
 };
